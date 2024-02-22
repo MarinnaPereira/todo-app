5d2e5220b1baa735a9a227a04d436116946181c1
MarinnaPereira <marinna.dos.anjos@gmail.com>
2024-02-22 14:42:57 +0100

    updated config files and .env

---
+++ client/.eslintrc.ts
@@ -0,0 +1,22 @@
+import { Linter } from "eslint";
+
+const config: Linter.Config = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
+
+export default config;
