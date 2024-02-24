3f406ab00f8ad1ca9a403ca62e966af3cb594e67
MarinnaPereira <marinna.dos.anjos@gmail.com>
2024-02-24 12:45:20 +0100

    testing render env variable

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,12 +1,7 @@
 import { TodoModel } from "../types/todoModels";
 import { TodosResModel, TodoResModel } from "../types/resModel";
 
-const isLive: boolean = false;
-
-const baseURL: string = isLive
-  ? "https://easy-todo-app-server.onrender.com"
-  : "http://localhost:4000";
-// const baseURL: string = "http://localhost:4000";
+let baseURL: string = "http://localhost:4000";
 // const baseURL: string = "https://easy-todo-app-server.onrender.com";
 
 export const getTodos = async (): Promise<TodosResModel> => {
